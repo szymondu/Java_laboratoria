5abce459eecd5b15723c22065c997167bc697f29
Szymon <Szymon.duda99@wp.pl>
2022-05-27 15:46:41 +0200

    Lab5 - Zad4

--- Lab4/src/sample/Pong.java
+++ Lab4/src/sample/Pong.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,7 +11,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-
+import javafx.util.Duration;
 
 public class Pong extends Application {
     private static final double WIDTH = 800;
@@ -22,25 +24,37 @@ public class Pong extends Application {
     private static final double ARENAX2 = ARENAX1 + ARENAWIDTH;
     private static final double ARENAY2 = ARENAY1 + ARENAHEIGHT;
     private static final double R =10;
+    private double x = ARENAX1 + ARENAWIDTH/2;
+    private double y = ARENAY1 + ARENAHEIGHT/2;
+    private double vy = 2;
+    private double vx = 5;
 
     @Override
-    public void start(Stage stage) throws Exception{
-        Canvas canvas = new Canvas(800, 600);
+    public void start(Stage stage) throws Exception {
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
+        Timeline t = new Timeline(new KeyFrame(Duration.millis(100), e -> run(gc)));
+        t.setCycleCount(Timeline.INDEFINITE);
         stage.setTitle("Kulki");
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
-
-        gc.setFill(Color.BLACK);
-        gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
-
-        gc.setFill(Color.WHITESMOKE);
-        gc.fillOval(ARENAX1+ARENAWIDTH/2, ARENAY1+ARENAHEIGHT/2,2*R, 2*R);
+        t.play();
     }
+        private void run (GraphicsContext gc){
+            gc.setFill(Color.BLACK);
+            gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
+            if ((x <= ARENAX1) || ((x >= ARENAX2))) vx = -vx;
+            if ((y <= ARENAY1) || ((y >= ARENAY2))) vy = -vy;
+
+            x += vx;
+            y += vy;
+
+            gc.setFill(Color.WHITESMOKE);
+            gc.fillOval(x, y, 2 * R, 2 * R);
+        }
 
 
-    public static void main(String[] args) {
-        launch(args);
+        public static void main (String[]args){
+            launch(args);
+        }
     }
-
-}
