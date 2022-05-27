631ba2a92e55027a2eff2f3e21097af75e7f628f
Szymon <Szymon.duda99@wp.pl>
2022-05-27 17:13:18 +0200

    Lab5 - Zad7

--- Lab5/src/pl/lublin/wsei/java/cwiczenia/console/TestBinCopy.java
+++ Lab5/src/pl/lublin/wsei/java/cwiczenia/console/TestBinCopy.java
@@ -1,19 +1,16 @@
 package pl.lublin.wsei.java.cwiczenia.console;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class TestBinCopy {
         public static void main(String[] args) throws IOException {
-            FileInputStream in = null;
-            FileOutputStream out = null;
+            BufferedInputStream in = null;
+            BufferedOutputStream out = null;
             long startTime = System.nanoTime();
 
             try{
-                in = new FileInputStream("sample_1920x1280.tiff");
-                out = new FileOutputStream("img_copy.tiff");
+                in = new BufferedInputStream(new FileInputStream("sample_1920x1280.tiff"));
+                out = new BufferedOutputStream(new FileOutputStream("img_copy.tiff"));
                 int c;
 
                 while ((c=in.read()) != -1){
