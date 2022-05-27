32f95de0e6d5f7954aa4f8aafcbdbdd2637ae056
Szymon <Szymon.duda99@wp.pl>
2022-05-27 15:34:43 +0200

    Lab5 - Zad1

--- Lab4/src/sample/Pong.java
+++ Lab4/src/sample/Pong.java
@@ -5,19 +5,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+
 
 public class Pong extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception{
+        Canvas canvas = new Canvas(800, 600);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        stage.setTitle("Kulki");
+        stage.setScene(new Scene(new StackPane(canvas)));
+        stage.show();
+
+        gc.setFill(Color.BLACK);
+        gc.fillRect(50,50,700,500);
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
