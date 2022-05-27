e9f94e4ae92df282c0a2a57655a5ed5db5280923
Szymon <Szymon.duda99@wp.pl>
2022-05-27 09:29:30 +0200

    Lab2 - Zad4

--- Lab2/src/sample/Controller.java
+++ Lab2/src/sample/Controller.java
@@ -1,4 +1,13 @@
 package sample;
-
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 public class Controller {
+    public Button btnMain;
+    int clickCounter = 0;
+
+    public void onBtnAction(ActionEvent actionEvent) {
+        System.out.println("Kliknięcie nr " + ++clickCounter);
+        if (clickCounter ==1)
+            btnMain.setText("I jeszcze raz");
+    }
 }
