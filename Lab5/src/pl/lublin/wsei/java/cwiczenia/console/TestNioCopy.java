bacc52dfddb55379789fb657f720c4bdcde6884c
Szymon <Szymon.duda99@wp.pl>
2022-05-27 17:34:57 +0200

    Lab5 - Zad9

--- Lab5/src/pl/lublin/wsei/java/cwiczenia/console/TestNioCopy.java
+++ Lab5/src/pl/lublin/wsei/java/cwiczenia/console/TestNioCopy.java
@@ -1,5 +1,6 @@
 package pl.lublin.wsei.java.cwiczenia.console;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,9 +11,9 @@ public class TestNioCopy {
     public static void main(String[] args) throws IOException{
         //long startTime = System.currentTimeMillis();
         long startTime = System.nanoTime();
-        Path orginal = Paths.get("sample_1920x1280.tiff");
-        Path kopia = Paths.get("img_copy2.tiff");
-        //Files.copy(orginal, kopia, StandardCopyOption.REPLACE_EXISTING);
+        File orginal = new File("sample_1920x1280.tiff");
+        File kopia = new File("img_copy2.tiff");
+        Files.copy(orginal.toPath(), kopia.toPath());
         //System.out.println("Czas kopiowania: "+ (System.currentTimeMillis()- startTime)+" ms");
         System.out.println("Czas kopiowania: "+ (float)(System.nanoTime()- startTime)/1000000+" ms");
     }
