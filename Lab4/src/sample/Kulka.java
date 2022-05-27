e2beeb98e161edd2650c5521a0d3db7e747d12b9
Szymon <Szymon.duda99@wp.pl>
2022-05-27 16:11:59 +0200

    Lab5 - Zad9

--- Lab4/src/sample/Kulka.java
+++ Lab4/src/sample/Kulka.java
@@ -9,23 +9,25 @@ public class Kulka {
     private double xSpeed;
     private double xPos;
     private double yPos;
+    private Color color;
 
     Kulka (double xPos, double yPos, double xSpeed, double ySpeed){
         this.xPos = xPos;
         this.yPos = yPos;
         this.xSpeed = xSpeed;
         this.ySpeed = ySpeed;
+        color = Color.WHITESMOKE;
     }
     public void checkBinaryCollision(double xLeft, double yTop, double xRight, double yBottom){
         if ((xPos - R <= xLeft) || (xPos + R >= xRight)){
             xSpeed =-xSpeed;
         }
-        if ((yPos - R <= xLeft) || (yPos + R >= xRight)){
+        if ((yPos - R <= yTop) || (yPos + R >= yBottom)){
             ySpeed =-ySpeed;
         }
     }
     public void draw(GraphicsContext gc){
-        gc.setFill(Color.WHITESMOKE);
+        gc.setFill(color);
         gc.fillOval(xPos - R, yPos - R, 2 * R, 2 * R);
     }
     public void update(){
